c761020db706c9a8d5c48a9358c40ce8b3cd52dc
Luis Felipe <202makertv@gmail.com>
2022-02-21 11:22:48 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import WebView from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1}}>
+        <StatusBar hidden />
+        <WebView source={{uri: "http://10.0.0.103/projeto-galeria/"}}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
